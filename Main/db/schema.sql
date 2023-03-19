d1b038831c97f7ce4fffdc889e6d1b2f96d0b095
daveS <davesimpcontrol@gmail.com>
2023-03-19 11:35:53 -0600

    finished first inquirer bracket, added the remaining pseudo code

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -5,7 +5,7 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(100)
 );
 
 CREATE TABLE role (
--- Main/index.js
+++ Main/index.js
@@ -74,6 +74,7 @@ function promptUser() {
 }
 promptUser();
 
+//PSEUDO CODE TO COMPLETE ___________________
 
 //SECOND INQUIRER SCREEN
 
@@ -81,7 +82,7 @@ promptUser();
 //   SHOW TABLE employee
 
 
-// option 2
+// addEmployee();
 //   What is the employees first name?
 //     insert text response into first_name
 
@@ -92,9 +93,12 @@ promptUser();
 //     show a list of all the role possibilities
 //       have the user clicked role add the role id to the employee key 
 
+//    What is their salary?
+//      user puts in a number over 5 digits and no more than 6 digits
+
 //   compile user response into a new employee with an auto generated id
 
-// option 3
+// updateEmployeeRole();
 //   Which employee are you changing the role of?
 //     show a scrollable list of all employees 
 
@@ -103,10 +107,10 @@ promptUser();
 
 //   update chosen employee's role_id
 
-// option 4
+// viewAllRoles();
 //   SHOW TABLE role
 
-// option 5
+// addRole();
 //   What role would you like to add?
 //     user text response creates title 
 
@@ -117,6 +121,24 @@ promptUser();
 //     update manager_id accordingly
   
 // compile user response
+
+// viewAllDepartments();
+//   SHOW TABLES department
+
+
+// addDepartment();
+//   "What is the name of the department you want to add"
+//     user adds a text response and it adds to the following schema 
+//     TABLE department (
+//       id INT PRIMARY KEY,
+//       name VARCHAR(30)
+//     );
+//       iwith the folloing seed.sql info 
+//       INSERT INTO department (id, name) VALUES
+      
+
+// ___________________
+
 
 
 
